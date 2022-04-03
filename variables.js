28bd43ffebbd955fedf5fb76108cb3c94ace6ea9
Voidlesh <98331236+Voidlesh@users.noreply.github.com>
2022-04-03 14:22:25 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+const i = 'hello world';
+console.log(i) //Constant Variable can't be reassigned hence the name const.
+let i = '';
+i = 'hello world';
+console.log(i) //Block scoped variable, can be reassigned.
+var i = '';
+i = 'hello world';
+console.log(i) //Var is used to make a normal variable, but both let and var work, I prefer let.
